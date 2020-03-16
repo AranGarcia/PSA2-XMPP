3a163b34926f8e3cb95b3a96428b26b176f771d5
AranGarcia <allan@apli.jobs>
2020-03-16 12:56:02 -0600

    Added create_users recipe.

--- Makefile
+++ Makefile
@@ -6,3 +6,6 @@ up:
 
 admin:
 	docker-compose exec ejabberdapp bin/ejabberdctl register admin localhost password
+
+create_users:
+	docker-compose exec ejabberdapp /home/ejabberd/create_users.sh
---
+++ ejabberd/create_users.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# User creation
+bin/ejabberdctl register user1 localhost password
+bin/ejabberdctl register user2 localhost password
+bin/ejabberdctl register user3 localhost password
